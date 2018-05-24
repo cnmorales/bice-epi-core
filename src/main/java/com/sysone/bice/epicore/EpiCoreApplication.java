b7f42755611b25fcc86d920bc6cd807b73b9832e
cnmorales <cnmorales2@gmail.com>
2018-05-24 12:22:14 -0300

    Initial Commit epi-core

---
+++ src/main/java/com/sysone/bice/epicore/EpiCoreApplication.java
@@ -0,0 +1,28 @@
+package com.sysone.bice.epicore;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class EpiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpiCoreApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner init(CarRepository repository) {
+		return args -> {
+			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+					"AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+				Car car = new Car();
+				car.setName(name);
+				repository.save(car);
+			});
+			repository.findAll().forEach(System.out::println);
+		};
+	}
+}
